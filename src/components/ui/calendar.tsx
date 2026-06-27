34dbe8b055c9ba0ffa65549759868ddc750ab10a
Deep Grewal <deepgrewal2600@gmail.com>
2026-06-27 02:09:37 +0000

    studio-7396336405:~/studio/android{main}$ ./gradlew clean
    > Configure p

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -51,11 +51,9 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        Chevron: (props) => {
-          if (props.orientation === "left") {
-            return <ChevronLeft className="h-4 w-4" />;
-          }
-          return <ChevronRight className="h-4 w-4" />;
+        Chevron: ({ orientation }) => {
+          if (orientation === "left") return <ChevronLeft className="h-4 w-4" />
+          return <ChevronRight className="h-4 w-4" />
         }
       }}
       {...props}
